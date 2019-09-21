11f116b61133070534dfc17a08bec733c466ae52
Keshow Sablaka <keshow.sablaka@freshworks.com>
2019-09-22 02:35:13 +0530

    Modified init of generator

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ import redis
 import settings
 import json
 
-REDIS_CON = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB_ID, password=settings.REDIS_PASSWORD)
-
 class EquityResults(object):
     @cherrypy.expose
     def index(self):
@@ -13,17 +11,21 @@ class EquityResults(object):
 
 @cherrypy.expose
 class EquityResultsWebService(object):
+
+    def __init__(self):  
+        self.redis_con = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB_ID, password=settings.REDIS_PASSWORD)
+    
     def GET(self, equity_name = None):
         results = []
-        equities_list = REDIS_CON.get("top_ten_equity")
+        equities_list = self.redis_con.get("top_ten_equity")
         equities_list = json.loads(equities_list)
         if equity_name:
             equity_name += "*"
-            equities_list = REDIS_CON.keys(equity_name)     
+            equities_list = self.redis_con.keys(equity_name)     
         for equity in equities_list:
-            result = {"name": equity, "equity_mini": REDIS_CON.get(equity)}
+            result = {"name": equity, "equity_mini": self.redis_con.get(equity)}
             results.append(result)
-        response = json.dumps({"last_updated_on": REDIS_CON.get("last_updated_on"), "results_mini": results})
+        response = json.dumps({"last_updated_on": self.redis_con.get("last_updated_on"), "results_mini": results})
         return response
 
 if __name__ == '__main__':
